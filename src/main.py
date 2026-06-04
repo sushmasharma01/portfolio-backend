9841ef378f32953e2df4c7a8222d2de7e63e3969
Sushma Sharma <sushma.sharma@sharajman.com>
2026-06-04 15:59:45 +0530

    added url of frontend

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
-
+import os
 from src.modules.contact.router import router as contact_router
 from fastapi.middleware.cors import CORSMiddleware
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = FastAPI(title="Portfolio Backend")
 
+frontend_url = os.getenv("FRONTEND_URL")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*",  # Angular dev server
-    ],
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
